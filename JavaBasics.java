3dcec728fe241fca0a6ced81d8c1e6dfeb6a2015
M-Manohar-Reddy30 <manoharreddyind@gmail.com>
2026-05-26 12:03:03 +0530

    Average of three numbers and input from the user

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,6 +1,17 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        System.out.println("Hello, Java!");
+import java.util.*;
 
+public class JavaBasics {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+
+       float total = pencil+pen+eraser;
+
+       float gst = total/100*18;
+       float TotalCost = total+gst;
+
+        System.out.println(TotalCost);
     }
 }
---
+++ variable and datatype/AverageOfThreeNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AverageOfThreeNumbers {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a+b+c;
+        int average = sum/3;
+
+        System.out.println(average);
+    }    
+}
