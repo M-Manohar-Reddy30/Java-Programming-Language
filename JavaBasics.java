caa9f31f3ea19bd38e894895ecdab2629cc2767b
M-Manohar-Reddy30 <manoharreddyind@gmail.com>
2026-05-26 12:04:51 +0530

    Average of three numbers and input from the user

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,17 +1,5 @@
-import java.util.*;
-
 public class JavaBasics {
     public static void main(String arg[]){
-        Scanner sc = new Scanner(System.in);
-        float pencil = sc.nextFloat();
-        float pen = sc.nextFloat();
-        float eraser = sc.nextFloat();
-
-       float total = pencil+pen+eraser;
-
-       float gst = total/100*18;
-       float TotalCost = total+gst;
-
-        System.out.println(TotalCost);
+        System.out.println("Hello, Java!");
     }
 }
